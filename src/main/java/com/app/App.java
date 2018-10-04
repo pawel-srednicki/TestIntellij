3ce880637f56fef47d63d98e1832a1c8832f5578
pawel <pawel@gmail.com>
2018-10-04 21:50:03 +0200

    C3

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,5 +1,7 @@
 package com.app;
 
+import com.app.model.Person;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        Person p1 = new Person("ADAM", 30);
     }
 }
