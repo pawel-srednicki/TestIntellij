0850d6450d1007f22a980f7a8f8d2d68b7a09265
pawel <pawel@gmail.com>
2018-10-04 21:50:32 +0200

    C4

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -13,5 +13,8 @@ public class App
 
         System.out.println( "Hello World!" );
         Person p1 = new Person("ADAM", 30);
+
+        System.out.println(p1.toString());
+        System.out.println(p1.getAge());
     }
 }
